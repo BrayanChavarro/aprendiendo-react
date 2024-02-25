a6a3dc91653ba9359e849dbaeb652111d08ef7f5
Brayan Chavarro <brayanstik.21@outlook.com>
2024-02-25 14:45:47 -0500

    "Practice react"

---
+++ projects/02-tic-tac-toe/src/App.jsx
@@ -0,0 +1,25 @@
+const TURNS = {
+  X: "x",
+  O: "o",
+};
+
+const board = Array(9).fill(null);
+
+function App() {
+  return (
+    <main className="board">
+      <h1>Tres en raya</h1>
+      <section className="game">
+        {board.map((_, index) => {
+          return (
+            <div className="cell" key={index}>
+              <span className="cell_content">{index}</span>
+            </div>
+          );
+        })}
+      </section>
+    </main>
+  );
+}
+
+export default App
